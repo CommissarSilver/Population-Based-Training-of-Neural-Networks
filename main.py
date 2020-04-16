9e9879ccb11092ca5335f7f2e77ab21a971279ef
Ahura <fardaar54>
2020-04-16 10:26:55 +0430

    Initial commit, just getting things started

---
+++ main.py
@@ -0,0 +1,13 @@
+import numpy as np
+import tensorflow as tf
+
+
+class Worker:
+    def __init__(self, *args, **kwargs):
+        print()
+
+    # def step(self):
+    #
+    # def exploit(self):
+    #
+    # def explore(self):
