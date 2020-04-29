e2c335d29075bc04b9129002a7f7b866fc6a65d4
Ahrua Majdinasab <fardaar54>
2020-04-29 09:10:56 +0430

    Minor changes.

--- DQN/DOOM!/sumtree.py
+++ DQN/DOOM!/sumtree.py
@@ -8,10 +8,13 @@ class Node:
         self.right_node = right_node
         self.is_leaf = is_leaf
         self.content = content
+
         if not self.is_leaf:
             self.value = self.left_node.value + self.right_node.value
+
         self.parent = None
         self.index = index
+
         if left_node is not None:
             left_node.parent = self
         if right_node is not None:
@@ -21,6 +24,7 @@ class Node:
     def create_leaf(cls, experience, value, index):
         leaf = cls(None, None, experience, is_leaf=True, index=index)
         leaf.value = value
+
         return leaf
 
 
@@ -28,15 +32,18 @@ def create_tree(memory: dict):
     values = [memory[i]['error'] for i in memory.keys()]
     nodes = [Node.create_leaf(memory[index], value, index) for index, value in enumerate(values)]
     leaf_nodes = nodes
+
     while len(nodes) > 1:
         inodes = iter(nodes)
         nodes = [Node(*pair) for pair in zip(inodes, inodes)]
+
     return nodes[0], leaf_nodes
 
 
 def retrieve(value: float, node: Node):
     if node.is_leaf:
         return node
+
     if node.left_node.value >= value:
         return retrieve(value, node.left_node)
     else:
