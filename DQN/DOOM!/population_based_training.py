570c22652b3e753ff282aedf51f2f3a339ad0f68
Ahrua Majdinasab <fardaar54>
2020-05-12 08:25:40 +0430

    Tidying up the folder

--- DQN/DOOM!/population_based_training.py
+++
@@ -1,7 +0,0 @@
-import DQN
-
-
-class Worker:
-    def __init__(self, state_shape, possible_actions, agent_hyper_parameters):
-        self.agent = DQN.DQN(state_shape=state_shape, hyper_parameters=agent_hyper_parameters,
-                             possible_actions=possible_actions)
