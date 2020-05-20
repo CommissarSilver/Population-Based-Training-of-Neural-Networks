5af79a458b1cf4c540bd4f50f8c2da21cce321e8
Ahura <fardaar54@gmail.com>
2020-05-20 19:28:53 +0430

    couple of improvements

--- Agent Architectures/A2C With Memory/A2C.py
+++ Agent Architectures/A2C With Memory/A2C.py
@@ -10,56 +10,56 @@ from collections import deque
 class Agent:
     def __init__(self, input_shape, possible_actions, hyper_params):
         self.model = model.build_model(input_shape, len(possible_actions))  # the agent's neural network
-        self.discount_factor = hyper_params['discount_factor']
-        self.learning_rate = hyper_params['learning_rate']
-        self.memory_size = hyper_params['memory_size']
-        self.train_steps = hyper_params['train_steps']
+        self.hyper_params = hyper_params
+        self.discount_factor = self.hyper_params['discount_factor']
+        self.learning_rate = self.hyper_params['learning_rate']
         self.optimizer = tf.keras.optimizers.RMSprop(self.learning_rate)
-        self.minions = [Minion(self) for i in range(hyper_params['minions_num'])]
+        self.minions = [Minion(self) for i in range(self.hyper_params['minions_num'])]
 
-    def train(self):
-        minion_threads = [threading.Thread(target=minion.play) for minion in self.minions]
-        for minion_thread in minion_threads:
-            minion_thread.start()
-        for minion_thread in minion_threads:
-            minion_thread.join()
-        experiences = [minion.memory for minion in self.minions]
-        losses=[]
-        print(' Training')
-        for experience in experiences:
-            # steps_num = len(self.experiences[episode_id]['rewards']) % self.step_size
+    def train(self, training_iters,id):
+        avg_returns = []
 
-            returns = np.zeros((len(experience['rewards']), 1))
-            ret = 0
-            for r in reversed(range(len(experience['rewards']))):  # calculate discounted rewards for the entire episode
-                ret = experience['rewards'][r] + self.discount_factor * ret
-                returns[r] = ret
-            returns = tf.linalg.normalize(returns)[0].numpy()
+        for i in range(training_iters):
+            print('     -> Worker {} Training Iteration {}'.format(id,i))
+            minion_threads = [threading.Thread(target=minion.play) for minion in self.minions]
 
-            with tf.GradientTape() as tape:
-                state_values, action_logprobs = self.model(np.asarray(experience['states']))
-                actor_loss = -1 * tf.multiply(action_logprobs,
-                                              tf.math.subtract(tf.stop_gradient(state_values), returns))
-                critic_loss = tf.math.pow(tf.math.subtract(state_values, returns), 2)
-                loss = tf.reduce_sum(actor_loss) + tf.multiply(0.1, tf.reduce_sum(critic_loss))
-                losses.append(loss)
-        variables = self.model.trainable_variables
-        gradients = tape.gradient(losses, variables)
-        self.optimizer.apply_gradients(zip(gradients, variables))
-        for minion in self.minions:
-            minion.update(self.model)
-        return np.average(returns)
+            for minion_thread in minion_threads:
+                minion_thread.start()
 
-    def test(self):
-        # test the agent to see how well it performs
-        for minion in self.minions:
-            minion.model.set_weights(self.model.get_weights())
-        minion_threads = []
+            for minion_thread in minion_threads:
+                minion_thread.join()
 
-        for minion in self.minions:
-            thread = threading.Thread(target=minion.play)
-            minion_threads.append(thread)
-            thread.start()
+            experiences = [minion.memory for minion in self.minions]
+            losses = []
+
+            for experience in experiences:
+                returns = np.zeros((len(experience['rewards']), 1))
+                ret = 0
+
+                for r in reversed(
+                        range(len(experience['rewards']))):  # calculate discounted rewards for the entire episode
+                    ret = experience['rewards'][r] + self.discount_factor * ret
+                    returns[r] = ret
+                returns = tf.linalg.normalize(returns)[0].numpy()
+
+                with tf.GradientTape() as tape:
+                    state_values, action_logprobs = self.model(np.asarray(experience['states']))
+                    actor_loss = -1 * tf.multiply(action_logprobs,
+                                                  tf.math.subtract(tf.stop_gradient(state_values), returns))
+                    critic_loss = tf.math.pow(tf.math.subtract(state_values, returns), 2)
+                    loss = tf.reduce_sum(actor_loss) + tf.multiply(0.1, tf.reduce_sum(critic_loss))
+                    losses.append(loss)
+
+            variables = self.model.trainable_variables
+            gradients = tape.gradient(losses, variables)
+            self.optimizer.apply_gradients(zip(gradients, variables))
+
+            for minion in self.minions:
+                minion.update(self.model)
+
+            avg_returns.append(np.average(returns))
+
+        return np.mean(avg_returns)
 
 
 class Minion:
@@ -102,8 +102,7 @@ class Minion:
 
 def main(train_or_test):
     master_agent = Agent((84, 84, 4), [[1, 0, 0], [0, 1, 0], [0, 0, 1]],
-                         {'learning_rate': 0.9, 'discount_factor': 0.95, 'memory_size': 10, 'train_steps': 10,
-                          'minions_num': 5})
+                         {'learning_rate': 0.9, 'discount_factor': 0.95, 'minions_num': 5})
     # master_agent.model.load_weights('Master-10.h5')
     print('     loading model')
 
@@ -113,6 +112,3 @@ def main(train_or_test):
     # #
     master_agent.model.save('Master-10.h5')
     print('     model saved')
-
-
-# main('train')
