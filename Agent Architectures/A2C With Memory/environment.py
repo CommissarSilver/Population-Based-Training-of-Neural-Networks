0b10a27fd14296fdcbcdb5d04ad1605caad1c1d7
ahura <a.majdinasab@hotmail.com>
2020-07-22 10:52:44 +0430

    Reformatting

--- Agent Architectures/A2C With Memory/environment.py
+++ Agent Architectures/A2C With Memory/environment.py
@@ -10,6 +10,7 @@ def create_environment(env_name='Breakout-v0'):
     environment = gym.make(env_name)
     observation_space = environment.observation_space
     action_space = environment.action_space
+
     return environment, observation_space, action_space
 
 
@@ -17,17 +18,21 @@ def test_environment():
     environment = gym.make('Breakout-v0')
     observation_space = environment.observation_space
     action_space = environment.action_space
+
     print(environment.unwrapped.get_action_meanings())
     print('Observation space: ', observation_space)
     print('Action space: ', action_space)
+
     observation = environment.reset()
     state = deque([np.zeros((84, 84)) for _ in range(4)], maxlen=4)
     stacked_frames = stack_frames.stack_frames(state, observation, True)
+
     for _ in range(1000):
         environment.render()
         observation, reward, done, info = environment.step(environment.action_space.sample())
         stacked_frames = stack_frames.stack_frames(stacked_frames, observation, False)
+
     environment.close()
 
 
-# test_environment()
+
