855aa4d3f2ee06faf685b39bcab24b09513bdd0b
Ahura Majdinasab <a.majdinasab@hotmail.com>
2020-09-11 21:42:53 +0430

    Create networks.py

---
+++ Agent Architectures/DDPG/networks.py
@@ -0,0 +1,50 @@
+import tensorflow as tf
+import os
+
+
+class CriticNetwork(tf.keras.Model):
+    def __init__(self, n_actions, fc1_dims=512, fc2_dims=512, name='critic', chkpt_dir='tmp/ddpg'):
+        super(CriticNetwork, self).__init__()
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.n_actions = n_actions
+
+        self.model_name = name
+        self.checkpoint_dir = chkpt_dir
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, self.model_name + '_ddpg.h5')
+
+        self.fc1 = tf.keras.layers.Dense(self.fc1_dims, activation='relu')
+        self.fc2 = tf.keras.layers.Dense(self.fc2_dims, activation='relu')
+        self.q = tf.keras.layers.Dense(1, activation=None)
+
+    def call(self, state, action):
+        action_value = self.fc1(tf.concat([state, action], axis=1))
+        action_value = self.fc2(action_value)
+
+        q = self.q(action_value)
+
+        return q
+
+
+class ActorNetwork(tf.keras.Model):
+    def __init__(self, n_actions=2, fc1_dims=512, fc2_dims=512, name='actor', chkpt_dir='tmp/ddpg'):
+        super(ActorNetwork, self).__init__()
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.n_actions = n_actions
+
+        self.model_name = name
+        self.checkpoint_dir = chkpt_dir
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, self.model_name + '_ddpg.h5')
+
+        self.fc1 = tf.keras.layers.Dense(self.fc1_dims, activation='relu')
+        self.fc2 = tf.keras.layers.Dense(self.fc2_dims, activation='relu')
+        self.mu = tf.keras.layers.Dense(self.n_actions, activation='tanh')
+
+    def call(self, state):
+        prob = self.fc1(state)
+        prob = self.fc2(prob)
+
+        mu = self.mu(prob)
+
+        return mu
