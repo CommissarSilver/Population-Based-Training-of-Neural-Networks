6e791816013fad6862af324aefe0f2e1a2016f5c
Ahura Majdinasab <a.majdinasab@hotmail.com>
2020-09-18 11:02:31 +0430

    Changed variable names, updated to match the other agent's style

--- Agent Architectures/DDPG/network.py
+++ Agent Architectures/DDPG/network.py
@@ -3,23 +3,22 @@ import os
 
 
 class CriticNetwork(tf.keras.Model):
-    def __init__(self, n_actions, fc1_dims=512, fc2_dims=512, name='critic', chkpt_dir='tmp/ddpg'):
+    def __init__(self, observation_dims, output_dims, name='critic', chkpt_dir='tmp/ddpg'):
         super(CriticNetwork, self).__init__()
-        self.fc1_dims = fc1_dims
-        self.fc2_dims = fc2_dims
-        self.n_actions = n_actions
+        self.input_dims = observation_dims
+        self.output_dims = output_dims
 
         self.model_name = name
         self.checkpoint_dir = chkpt_dir
         self.checkpoint_file = os.path.join(self.checkpoint_dir, self.model_name + '_ddpg.h5')
 
-        self.fc1 = tf.keras.layers.Dense(self.fc1_dims, activation='relu')
-        self.fc2 = tf.keras.layers.Dense(self.fc2_dims, activation='relu')
-        self.q = tf.keras.layers.Dense(1, activation=None)
+        self.dense_layer_1 = tf.keras.layers.Dense(units=512, activation='relu', name='Dense_Layer_1', dtype=tf.float32)
+        self.dense_layer_2 = tf.keras.layers.Dense(units=512, activation='relu', name='Dense_Layer_2', dtype=tf.float32)
+        self.q = tf.keras.layers.Dense(units=1, activation=None, name='Qs', dtype=tf.float32)
 
     def call(self, state, action):
-        action_value = self.fc1(tf.concat([state, action], axis=1))
-        action_value = self.fc2(action_value)
+        action_value = self.dense_layer_1(tf.concat([state, action], axis=1))
+        action_value = self.dense_layer_2(action_value)
 
         q = self.q(action_value)
 
@@ -27,23 +26,22 @@ class CriticNetwork(tf.keras.Model):
 
 
 class ActorNetwork(tf.keras.Model):
-    def __init__(self, n_actions=2, fc1_dims=512, fc2_dims=512, name='actor', chkpt_dir='tmp/ddpg'):
+    def __init__(self, observation_dims, output_dims, name='actor', chkpt_dir='tmp/ddpg'):
         super(ActorNetwork, self).__init__()
-        self.fc1_dims = fc1_dims
-        self.fc2_dims = fc2_dims
-        self.n_actions = n_actions
+        self.input_dims = observation_dims
+        self.output_dims = output_dims
 
         self.model_name = name
         self.checkpoint_dir = chkpt_dir
         self.checkpoint_file = os.path.join(self.checkpoint_dir, self.model_name + '_ddpg.h5')
 
-        self.fc1 = tf.keras.layers.Dense(self.fc1_dims, activation='relu')
-        self.fc2 = tf.keras.layers.Dense(self.fc2_dims, activation='relu')
-        self.mu = tf.keras.layers.Dense(self.n_actions, activation='tanh')
+        self.dense_layer_1 = tf.keras.layers.Dense(units=512, activation='relu', name='Dense_Layer_1', dtype=tf.float32)
+        self.dense_layer_2 = tf.keras.layers.Dense(units=512, activation='relu', name='Dense_Layer_2', dtype=tf.float32)
+        self.mu = tf.keras.layers.Dense(units=self.output_dims, activation='tanh', name='Mu', dtype=tf.float32)
 
     def call(self, state):
-        prob = self.fc1(state)
-        prob = self.fc2(prob)
+        prob = self.dense_layer_1(state)
+        prob = self.dense_layer_2(prob)
 
         mu = self.mu(prob)
 
