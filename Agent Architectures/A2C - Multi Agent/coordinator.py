5e65663a89e523c2e693f2b9727c51192c3bb86a
Ahura <a.majdinasab@hotmail.com>
2021-01-07 12:09:34 +0330

    Experimenting with different architectures

--- Agent Architectures/A2C - Multi Agent/coordinator.py
+++ Agent Architectures/A2C - Multi Agent/coordinator.py
@@ -3,6 +3,7 @@ from agent import Agent
 import gym
 import ma_gym
 import numpy as np
+import random
 
 
 class Coordinator:
@@ -24,10 +25,10 @@ class Coordinator:
         self.total_reward = 0
         self.episode_finished = False
 
-    def play(self, unroll_length=20, show_env=False):
+    def play(self, unroll_length=2, show_env=False):
         self.episode_finished = False
         steps = 0
-        while not self.episode_finished:
+        while (steps < unroll_length and not self.episode_finished):
             actions = []
 
             for i, observation in enumerate(self.observation):
@@ -51,31 +52,36 @@ class Coordinator:
                 self.episode_rewards.append(self.total_reward)
                 self.total_reward = 0
                 self.mean_scores = np.mean(self.episode_rewards)
-                f = open('{}-Single-5000.csv'.format(self.environment_name), 'a')
-                f.write('{},{},{}\n'.format(self.id, self.episode_number, self.episode_rewards[-1]))
+                for agent in self.agents:
+                    f = open('{}-Non PBT.csv'.format('Checkers-v0'), 'a')
+                    f.write(
+                        '{},{},{},{},{},{},{},{}\n'.format(self.id, self.episode_number, agent.id,
+                                                           np.sum(agent.rewards),
+                                                           agent.hyper_parameters['learning_rate'],
+                                                           agent.hyper_parameters['entropy_coefficient'],
+                                                           agent.hyper_parameters['critic_coefficient'],
+                                                           self.episode_rewards[-1]))
+                    f.close()
 
             steps += 1
 
         for agent in self.agents:
             agent.learn()
-        #     for agent in self.agents:
-        #         f = open('{}-PBT.csv'.format(self.environment_name), 'a')
-        #         f.write(
-        #             '{},{},{},{},{},{}\n'.format(self.id, agent.id, agent.network.optimizer.learning_rate.numpy(),
-        #                                          self.episode_number,
-        #                                          np.sum(agent.rewards),
-        #                                          total_score))
-        #
-        #         agent.learn()
-        #         # if episode_number % 100 == 0:
-        #         agent.save_models()
-        #     scores.append(total_score)
-        #     print('{} episode {} finished'.format(self.id, self.episode_number))
 
 
-env1 = Coordinator('Checkers-v0',
-                   {'learning_rate': 0.0001, 'discount_factor': 0.99, 'unroll_length': 50, 'minions_num': 5},
-                   coordinator_id=0)
-while env1.episode_number<5000:
-    env1.play()
-    print(env1.episode_number)
+f = open('{}-Non PBT.csv'.format('Checkers-v0'), 'a')
+f.write('Coordinator ID,Episode Number,Agent ID,Agent Reward,Agent Learning Rate,Agent Entropy, Agent Critic '
+        'Coefficient,Total Reward\n')
+f.close()
+coordinator1 = Coordinator('Checkers-v0',
+                           {'learning_rate': 0.00001,
+                            'entropy_coefficient': 0.001,
+                            'critic_coefficient': 0.3,
+                            'discount_factor': 1,
+                            'unroll_length': 5,
+                            'minions_num': 5},
+                           coordinator_id=0)
+
+while coordinator1.episode_number < 2000:
+    coordinator1.play()
+    print(coordinator1.episode_number)
